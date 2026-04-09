142c28d65fda1bd77a1c87223ca865b3adf6d0ea
srilu8790 <uppalapatisrilakshmi2000@gmail.com>
2026-04-08 23:23:06 -0500

    Initial Node app setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello from CI/CD project 🚀")
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
